66aba0d78e6173319284efb3cdc8d13002e8386d
nageld1 <derek.nagel@state.mn.us>
2025-02-13 09:07:02 -0600

    Fix issue with get_workbook_id

--- R/get_workbook_id.R
+++ R/get_workbook_id.R
@@ -5,4 +5,5 @@ get_workbook_id <- function(server, name) {
   if(length(id) > 1) stop(paste0(
     'Multiple workbooks found with name "', name, '". Use a unique workbbok name or the parent_project argument to choose a specific project.')
   )
+  return(id)
 }
